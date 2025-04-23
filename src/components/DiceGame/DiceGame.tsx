7b8f76a12db74323991f3b58a5f3975a083ac122
Briws <brightwiths@gmail.com>
2025-04-24 01:41:03 +0700

    Added marks points to Slider every 20 values

--- src/components/DiceGame/DiceGame.tsx
+++ src/components/DiceGame/DiceGame.tsx
@@ -50,14 +50,12 @@ const DiceGame: React.FC = () => {
   }
 
   const marks = [
-    {
-      value: 0,
-      label: '0',
-    },
-    {
-      value: 100,
-      label: '100',
-    },
+    { value: 0, label: '0' },
+    { value: 20, label: '' },
+    { value: 40, label: '' },
+    { value: 60, label: '' },
+    { value: 80, label: '' },
+    { value: 100, label: '100' },
   ]
 
   return (
